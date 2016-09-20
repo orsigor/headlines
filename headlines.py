14503ae71a5c403de2224166be719d5125e71e12
orsigor <igor.manzoni@gmail.com>
2016-09-20 17:15:07 +0200

    with jinja templates

--- headlines.py
+++ headlines.py
@@ -1,12 +1,15 @@
 import feedparser
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 RSS_FEEDS={'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 	   'cnn':'http://rss.cnn.com/rss/edition.rss',
 	   'fox':'http://feeds.foxnews.com/foxnews/latest',
-	   'iol':'http://www.iol.co.za/cmlink/1.640'
+	   'iol':'http://www.iol.co.za/cmlink/1.640',
+	   'cor':'http://xml.corriereobjects.it/rss/homepage.xml',
+	   'mil':'http://xml.corriereobjects.it/rss/homepage_milano.xml',
+	   'ans':'http://www.ansa.it/main/notizie/awnplus/topnews/synd/ansait_awnplus_topnews_medsynd_Today_Idx.xml',
 	  }
 
 
@@ -15,15 +18,7 @@ RSS_FEEDS={'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
 	feed=feedparser.parse(RSS_FEEDS[publication])
 	first_article=feed['entries'][0]
-	return """<html>
-		<body>
-			<h1>Headlines</h1>
-			<b>{0}</b><br/>
-			<b>{1}</b><br/>
-			<p>{2}</p><br/>
-		</body>
-		</html>""".format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
-
+	return render_template("home.html",articles=feed['entries'])
 
 if __name__=='__main__':
 	app.run(host="172.16.0.58",port=5000,debug=True)
